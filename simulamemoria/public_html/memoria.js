79a74f665716ec8051477d3765b3811b6b8c59ea
Gabriel Ramos <gabriel.ramos@nitrodev.com>
2016-06-16 18:01:31 -0300

    generalização da classe
    generalização da classe e argumento para função de callback

--- simulamemoria/public_html/memoria.js
+++ simulamemoria/public_html/memoria.js
@@ -3,23 +3,23 @@
 
 window.onload = function iniciaMemoria()
 {
-    function ControladorCache(tamanhoCache){
-        this.tamanhoCache = tamanhoCache,
-        this.memoriaCache = [],
-        this.populaCache = function(){
-                        for(var x = 0; x < this.tamanhoCache; x++)
+    function Memoria(tamanho){
+        this.tamanhoMemoria = tamanho,
+        this.memoria = [],
+        this.popula = function(){
+                        for(var x = 0; x < this.tamanhoMemoria; x++)
                         {
-                            this.memoriaCache[x] = [];
-                            for (var y=0; y < this.tamanhoCache; y++)
-                                this.memoriaCache[x][y] = y;
+                            this.memoria[x] = [];
+                            for (var y=0; y < this.tamanhoMemoria; y++)
+                                this.memoria[x][y] = y;
                         }
-                            },
-        this.populaCache(),
+        },
+        this.popula(),
         this.montaTabela = function()
         {
             var tabela = '';
-            tabela += '<table id="tabelaCache">';
-            this.memoriaCache.forEach(function(valorMemoria, index, pagina)
+            tabela += '<table id="tabelaCache" border="1">';
+            this.memoria.forEach(function(valorMemoria, index, pagina)
             {   
 	            tabela += '<tr>';
 	            valorMemoria.forEach(function(valorPosicao)
@@ -30,15 +30,44 @@ window.onload = function iniciaMemoria()
             });
             tabela += '</table>';
         	return tabela;
+        },
+        this.verificaPosicao = function(posicao)
+        {
+            var flag = [];
+            posicao.forEach(function(valor, index)
+            {
+                if(valor <= this.tamanhoMemoria)
+                    flag[index] = 1;
+                console.log('Primeiro foreach --tamanho da memoria-- '+this.tamanhoMemoria);  
+            },this);
+            var soma;
+            flag.forEach(function(valor, index)
+            {
+                soma = flag[index];
+            });
+            console.log('Soma '+soma);
+            if((soma / flag.length) == 1)
+                return true;
+            else
+                return false;
+        }
+        this.verificaMemoria = function(posicao)
+        {
+            if(this.verificaPosicao(posicao))
+                return this.memoria[posicao[0]][posicao[1]];
+            else
+                return null;
         };
 
     } 
     
-    var controlador = new ControladorCache(5);
-    
-    console.log(controlador.memoriaCache);
+    var cache = new Memoria(10);
 
-    console.log(controlador.montaTabela());
+    console.log(cache.tamanhoMemoria);
+
+    console.log(cache.verificaMemoria(Array(1,1)));
+
+
     
-	divCache.innerHTML = controlador.montaTabela();
+	divCache.innerHTML = cache.montaTabela();
 };
