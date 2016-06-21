bf599338bdac7f149b0e387df658454d4d5ab9b9
marcosvlp <marcosvlp@P025527>
2016-06-21 20:04:37 -0300

    finalização para apresentação

--- simulamemoria/public_html/memoria.js
+++ simulamemoria/public_html/memoria.js
@@ -6,37 +6,25 @@ window.onload = function iniciaSimulacao()
             divLogica.innerHTML = memorias[0].montaTabela();
             divCache.innerHTML = memorias[1].montaTabela();
             divRAM.innerHTML = memorias[2].montaTabela();
-            divProcessador.innerHTML = '<table id="tabelaCache"><tr><td>' + "Nenhuma palavra" + '<td><tr></table>';
+            divProcessador.innerHTML = '<table><tr><td>' + "Nenhuma palavra" + '</td></tr></table>';
         },
         this.atualiza = function(){
             divRAM.innerHTML = ram.montaTabela();
+			hits.innerHTML = logica.hit;
+			miss.innerHTML = logica.miss;
             setTimeout(function(){
-                console.log("teste");
                 divCache.innerHTML = cache.montaTabela();
-                divProcessador.innerHTML = '<table id="tabelaCache"><tr><td>' + processador.palavraAtual + '<td><tr></table>';
+                divProcessador.innerHTML = '<table><tr><td>' + processador.palavraAtual + '</td></tr></table>';
             }, 2000);
         };
     }
     
     //OBJETO MEMORIA
-    function Memoria(logica,tamanho){
-        this.logica,
+    function Memoria(hierarquia,tamanho){
+        this.hierarquia = hierarquia,
         this.tamanhoMemoria = tamanho,
         this.memoria = [],
         this.popula = function(){
-            if(logica){
-                for(var x = 0; x < this.tamanhoMemoria; x++)
-                {
-                    //MMU MAPEAMENTO
-                    this.memoria[x] = [];
-                    for (var y=0; y < this.tamanhoMemoria; y++){
-                        this.memoria[x][y] = [""];
-                        for(z = 0; z<2; z++){
-                            this.memoria[x][y][0] += (Math.floor((Math.random() * 3))).toString();
-                        }
-                    }
-                }
-            }else{
                 for(var x = 0; x < this.tamanhoMemoria; x++)
                 {
                     this.memoria[x] = [];
@@ -49,11 +37,10 @@ window.onload = function iniciaSimulacao()
                     }
                 }
             }
-        },
         this.popula(),
         this.montaTabela = function(){
             var tabela = '';
-            tabela += '<table id="tabelaCache" border="1">';
+            tabela += '<table id="tabelaCache" border="1" style="width: 100%">';
             for(var i in this.memoria)
             {   
                 tabela += '<tr>';
@@ -66,21 +53,6 @@ window.onload = function iniciaSimulacao()
             return tabela;
         },
         
-        this.escreveCache = function(pos,dado){
-            var validade = false;
-            var posFisica = new Array(this.memoria[pos[0]][pos[1]][0].charAt(0),this.memoria[pos[0]][pos[1]][0].charAt(1));
-            var blocoMemoria = cache.memoria[posFisica[0]][posFisica[1]][0];
-            
-            if(blocoMemoria.charAt(0) == 1){
-                console.log("blocoMemoria - "+blocoMemoria);
-                validade = true;
-                cache.memoria[posFisica[0]][posFisica[1]][0] = dado;
-            }else{
-                //MISS?
-            }
-            
-            return validade;
-        },
         
         this.retornaProcessador = function(pos){
             var validade = false;
@@ -92,10 +64,17 @@ window.onload = function iniciaSimulacao()
                 validade = true;
             }else{
                 var novoBloco = this.resgataRAM(posFisica);
+                novoBloco = this.substituiChar(novoBloco,0,"1");
                 processador.palavraAtual = novoBloco;
                 cache.memoria[posFisica[0]][posFisica[1]][0] = novoBloco;
             }
             return validade;
+        },
+        
+        function substituiChar(str,pos,char){
+        	p1 = str.substring(0,pos-1);
+        	p2 =str.substring(pos,str.length);
+        	return p1+char+p2;
         },
         
         this.resgataRAM = function(pos){
@@ -118,6 +97,8 @@ window.onload = function iniciaSimulacao()
 
 
     function Logica(){
+		this.hit = 0,
+		this.miss = 0,
         this.memoria = [],
         this.populaLogica = function(){
             var cont = Number();
@@ -134,6 +115,7 @@ window.onload = function iniciaSimulacao()
                 }
             }
         },
+        
         this.montaTabela = function(){
             var tabela = '';
             tabela += '<table id="tabelaCache" border="1">';
@@ -143,7 +125,7 @@ window.onload = function iniciaSimulacao()
                 {
                     tabela += '<tr>';
                     for(var k in this.memoria[i][j]){
-                        tabela += '<td>'+i+" "+j+" "+k+"|"+this.memoria[i][j][k]+'</td>';
+                        tabela += '<td>'+i+" "+j+" "+k+'</td>';
                 }
                 tabela += '</tr>';
                 }
@@ -151,65 +133,117 @@ window.onload = function iniciaSimulacao()
             tabela += '</table>';
             return tabela;
         },
+        
+        this.escreveCache = function(pos,dado){
+            var validade = false;
+            var blocoMemoria = cache.memoria[pos[0]][pos[1]][0];
+
+            cache.memoria[pos[0]][pos[1]][0] = dado+"";
+            
+            return validade;
+        },
+        
         this.retornaProcessador = function()
         {
-            console.log(this.memoria[arguments[0][0]][arguments[0][1]][arguments[0][2]]);
-            processador.palavraAtual = this.memoria[arguments[0][0]][arguments[0][1]][arguments[0][2]][0];
-
+			if(this.memoria[arguments[0][0]][arguments[0][1]][arguments[0][2]][0].charAt(0) == 0){
+				this.miss++;
+				processador.palavraAtual = "Palavra Inválida";
+			}else{
+				this.hit++;
+				processador.palavraAtual = this.memoria[arguments[0][0]][arguments[0][1]][arguments[0][2]][0];
+			}
+            return this.memoria[arguments[0][0]][arguments[0][1]][arguments[0][2]];
         }
     }
     
     //OBJETO PROCESSADOR 
     function Processador(){
         var palavraAtual;
-        this.atualizaMemoria = function()
+        var hit;
+        var miss;
+        this.atualizaMemoria = function(hierarquia, memoriaDestino, memoriaOrigem, dadoPos)
         {
-            if(arguments[0] == 1)
+            if(hierarquia > 0)
             {   //Recebe qual a posição possui o LRU na memória enviada para a função
-                var lru = this.verificaLRU(memorias[1]);
-
+                var lru = this.verificaLRU(memoriaDestino);
+                var dado = logica.retornaProcessador(dadoPos);
+                temp = memoriaDestino.memoria[lru[1]][lru[2]];
+                memoriaDestino.memoria[lru[1]][lru[2]] = dado;
+                memoriaOrigem.memoria[dadoPos[1]][dadoPos[2]] = temp;
             }
-
         }
         this.verificaLRU = function() //Varre o array de memórias passado como parâmetro, retornando a posição onde há o LRU
         {
             var lru = [Number.MIN_VALUE];
-            console.log(lru);
             for(var i in arguments[0])
                 for(var j in arguments[0][i])
                     for(var k in arguments[0][i][j])
-                    {
-                        console.log(arguments[0][i][j][k]);
                         if(arguments[0][i][j][k][1] > lru[0])
                         {
                             lru[0] = arguments[0][i][j][k][1]; //Valor do LRU
-                            lru[1] = k; //Coordenadas da matriz para a posição de LRU --precisa de debug
-                            lru[2] = j;
+                            lru[1] = j; //Coordenadas da matriz para a posição de LRU
+                            lru[2] = k; //idem acima
+                            lru[3] = arguments[0][i][j][k][0]; //valor do espaço de memoria
                         }
-                    }
-            console.log(lru);
             return lru;
+        }
+        this.aleatorio = function(){
+        	var vezes = 1000;
+        	var cont = 0;
+        	
+        	while(cont < vezes){
+        		setTimeout(function(){
+        			randomVect = new Array(Math.floor((Math.random() * 3)), Math.floor((Math.random() * 3)), Math.floor((Math.random() * 3)));
+        			if(randomVect[0] > 0)
+        	    	{
+        	    		processador.atualizaMemoria(ram.hierarquia, cache, ram, randomVect);
+        	    	}
+        	    	else
+        	    	{
+        	    		logica.retornaProcessador(randomVect);
+        	    	}
+        			console.log(cont);
+        	    	pagina.atualiza();
+                }, cont*10);
+        		cont++;
+        		console.log(cont<vezes);
+        		pagina.atualiza();
+        	}
+            
+            
         }
     }
 
+	
     var pagina = new Pagina();
     var processador = new Processador();
-    var memorias = [logica = new Logica(), cache = new Memoria(0,3), ram = new Memoria(0,3)];
+    var memorias = [logica = new Logica(), cache = new Memoria(0,3), ram = new Memoria(1,3)];
     
     logica.populaLogica(cache, ram);
-
-    processador.atualizaMemoria(1);
 
     pagina.inicializa();
     
     //USUÁRIO
     document.getElementById('botao').onclick = function() {
-        logica.retornaProcessador(Array(document.getElementById('posx').value,document.getElementById('posy').value,document.getElementById('posz').value));
-        pagina.atualiza();
+    	if(document.getElementById('posx').value == 1)
+    	{
+    		processador.atualizaMemoria(ram.hierarquia, cache, ram, Array(document.getElementById('posx').value,document.getElementById('posy').value,document.getElementById('posz').value));
+    	}
+    	else
+    	{
+    		logica.retornaProcessador(Array(document.getElementById('posx').value,document.getElementById('posy').value,document.getElementById('posz').value));
+    	}
+    		
+    	pagina.atualiza();
     }
     
     document.getElementById('botao2').onclick = function() {
-        logica.escreveCache(Array(document.getElementById('posx2').value,document.getElementById('posy2').value), document.getElementById('dado').value);
+    	logica.escreveCache(Array(document.getElementById('posx2').value,document.getElementById('posy2').value), document.getElementById('dado').value);
+        
         pagina.atualiza();
+    }
+    
+    document.getElementById('botao3').onclick = function() { 
+    	processador.aleatorio();
     }
 };
